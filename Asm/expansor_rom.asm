5a387ff996b872de2db2413693e8627840abd78d
Leonardo Montenegro <leonardo.montenegro@hp.com>
2023-10-11 10:10:02 -0300

    Acentuada a fonte do jogo, e implementada rotina pra expandir a rom para 16mb.

--- Asm/expansor_rom.asm
+++ Asm/expansor_rom.asm
@@ -1,8 +1,8 @@
 ; Script de expansão da rom pra 16mb
 ;
-; Escrito por Solid One - Setembro de 2023
+; Escrito por Solid One - Outobro de 2023
 .gba
-.open "Mega Man Battle Network 2 (BR).gba", 0x08000000
-.orga filesize("Mega Man Battle Network 2 (BR).gba")
-.fill 16777216 - filesize("Mega Man Battle Network 2 (BR).gba"), 0xff
+.open "Mega Man Battle Network 3 - Versao Azul (BR).gba", 0x08000000
+.orga filesize("Mega Man Battle Network 3 - Versao Azul (BR).gba")
+.fill 16777216 - filesize("Mega Man Battle Network 3 - Versao Azul (BR).gba"), 0xff
 .close
