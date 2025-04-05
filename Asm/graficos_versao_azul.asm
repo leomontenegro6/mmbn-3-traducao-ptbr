4910d2b21d589dd6f30d57d3885be176f0a48de5
Leonardo Montenegro <leomontenegro6@gmail.com>
2025-04-05 20:19:47 -0300

    Editada fonte grande, usadas nos menus "Customizar Navi" e "Rede".

--- Asm/graficos_versao_azul.asm
+++ Asm/graficos_versao_azul.asm
@@ -8,29 +8,33 @@
 ; Gráficos descomprimidos, em seus respectivos offsets.
 ; Fonte dos textos acentuada (sem VWF, usada em alguns menus)
 .org 0x08679B5C
-.incbin "Graficos/Editados/Fonte textos.gba"
+    .incbin "Graficos/Editados/Fonte textos.gba"
 
 ; Nome "Licenciado pela Nintendo", antes da tela-título.
 .org 0x087F9EB8
-.incbin "Graficos/Editados/Licenciado pela Nintendo.gba"
+    .incbin "Graficos/Editados/Licenciado pela Nintendo.gba"
+
+; Fonte grande negritada, usada nos menus "Rede" e "Customizar Navi"
+.org 0x08681BDC
+    .incbin "Graficos/Editados/Fonte grande.gba"
 
 ; Gráficos comprimidos, inseridos no final da rom.
 ; Catalogando ponteiros dos gráficos.
 .org 0x08022614
-.dw logo_tela_titulo
+    .dw logo_tela_titulo
 .org 0x08022618
-.dw nomes_tela_titulo
+    .dw nomes_tela_titulo
 
 ; Inserindo gráficos no final da rom
 .orga filesize("Mega Man Battle Network 3 - Versao Azul (BR).gba")
 .align
 
 logo_tela_titulo:
-.incbin "Graficos/Recomprimidos/Logo tela-titulo versao azul.gba"
-.align
+    .incbin "Graficos/Recomprimidos/Logo tela-titulo versao azul.gba"
+    .align
 
 nomes_tela_titulo:
-.incbin "Graficos/Recomprimidos/Nomes tela-titulo versao azul.gba"
-.align
+    .incbin "Graficos/Recomprimidos/Nomes tela-titulo versao azul.gba"
+    .align
 			
 .close
