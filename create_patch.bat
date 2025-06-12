a6ddc9966053018e9e9e2ec6da31a9c5d74b99e8
Leomontenegro <leomontenegro6@gmail.com>
2025-06-12 08:09:43 -0300

    Atualizada versão da tradução para 1.1.

--- create_patch.bat
+++ create_patch.bat
@@ -1,7 +1,7 @@
 @echo off
-del "[GBA] Mega Man Battle Network 3 - Versao Branca (U) (1.0).*"
-del "[GBA] Mega Man Battle Network 3 - Versao Azul (U) (1.0).*"
+del "[GBA] Mega Man Battle Network 3 - Versao Branca (U) (1.1).*"
+del "[GBA] Mega Man Battle Network 3 - Versao Azul (U) (1.1).*"
 cd ".\Ferramentas\"
-.\flips.exe -c "..\Mega Man Battle Network 3 - White Version (USA).gba" "..\Mega Man Battle Network 3 - Versao Branca (BR).gba" "..\[GBA] Mega Man Battle Network 3 - Versao Branca (U) (1.0).ips"
-.\flips.exe -c "..\Mega Man Battle Network 3 - Blue Version (USA).gba" "..\Mega Man Battle Network 3 - Versao Azul (BR).gba" "..\[GBA] Mega Man Battle Network 3 - Versao Azul (U) (1.0).ips"
-.\7z.exe a -tzip "..\[GBA] Mega Man Battle Network 3 (U) (1.0).zip" "..\[GBA] Mega Man Battle Network 3 - Versao Branca (U) (1.0).ips" "..\[GBA] Mega Man Battle Network 3 - Versao Azul (U) (1.0).ips" "..\LEIAME.txt"
+.\flips.exe -c "..\Mega Man Battle Network 3 - White Version (USA).gba" "..\Mega Man Battle Network 3 - Versao Branca (BR).gba" "..\[GBA] Mega Man Battle Network 3 - Versao Branca (U) (1.1).ips"
+.\flips.exe -c "..\Mega Man Battle Network 3 - Blue Version (USA).gba" "..\Mega Man Battle Network 3 - Versao Azul (BR).gba" "..\[GBA] Mega Man Battle Network 3 - Versao Azul (U) (1.1).ips"
+.\7z.exe a -tzip "..\[GBA] Mega Man Battle Network 3 (U) (1.1).zip" "..\[GBA] Mega Man Battle Network 3 - Versao Branca (U) (1.1).ips" "..\[GBA] Mega Man Battle Network 3 - Versao Azul (U) (1.1).ips" "..\LEIAME.txt"
